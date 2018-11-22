b87d2a6a4cdaa3030ed662ecc5531475bfe3ee32
Anni <32060255+eneaneli@users.noreply.github.com>
2018-11-22 11:16:08 +0100

    removed test from xunit

--- TestBusCompanyXUNIT/UnitTest1.cs
+++ TestBusCompanyXUNIT/UnitTest1.cs
@@ -7,15 +7,6 @@ namespace TestBusCompanyXUNIT
     public class UnitTest1
     {
 
-        //tests 500 km 100ppl
-        [Fact]
-        public void DistanceLongPeopleTest6XUNIT()
-        {
-            IBusCompany bus = new BusCompany();
-            var km = 500;
-            var ppl = 100;
-            var priceprkm = 2.25;
-            Assert.True(bus.TotalCost(ppl, km) == 130 + km * priceprkm);
-        }
+     
     }
 }
